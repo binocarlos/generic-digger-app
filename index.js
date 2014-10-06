b4869046e0a9912f262450f7b47c30095977e42e
Kai Davenport <kaiyadavenport@gmail.com>
2014-10-06 14:25:17 +0000

    basic look and feel

--- index.js
+++ index.js
@@ -2,23 +2,13 @@ var fs = require('fs')
 var path = require('path')
 
 var Database = require('./lib/database')
-var Session = require('./lib/session')
 var HTTP = require('./lib/httpserver')
 var Router = require('./lib/router')
-var Gandalf = require('./lib/gandalf')
-
 var log = require('./lib/log')('boot')
 var config = require('./config')
 
 var db = Database(config)
-var session = Session(db)
-var gandalf = Gandalf(config, db, session)
-
-var router = Router(config, {
-	db:db,
-	session:session,
-	gandalf:gandalf
-})
+var router = Router(config, db)
 
 HTTP(config, router, function(err){
 	log('started')
